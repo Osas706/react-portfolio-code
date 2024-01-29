bc75361b1405feaa355fb79a95d2f3fbd48acd77
Osas706 <92319443+Osas706@users.noreply.github.com>
2024-01-29 12:47:30 +0100

    done

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -4,6 +4,8 @@ import About from './components/about/About';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
 import Experience from './components/experience/Experience'
+import Project from './components/project/Project';
+import Contact from './components/contact/Contact';
 
 function App() {
 
@@ -12,7 +14,9 @@ function App() {
       <Navbar />
       <Hero />
       <About />
-     
+      <Experience />
+      <Project />
+      <Contact />
    </div>
   )
 }
--- portfolio/src/components/experience/Experience.jsx
+++ portfolio/src/components/experience/Experience.jsx
@@ -1,27 +1,28 @@
-import React from 'react';
-import skills from '../../data/skills.json';
-import { getImageUrl } from '../../utils';
+import React from "react";
+
+import styles from './Experience.module.css';
+import skills from "../../data/skills.json";
+import { getImageUrl } from "../../utils";
 
 const Experience = () => {
   return (
-    <section id="experience">
-       <h2>Experience</h2>
+    <section id="experience" className={styles.container}>
+      <h2 className={styles.title}>Experience</h2>
 
-       <div>
-         <div>
-          {
-            skills.map((skill, id) => {
-                <div key={id}>
-                   <div><img src={getImageUrl(skill.imageSrc)} alt={skill.title} /></div>
-                </div>
-            })
-          }
-         </div>
-
-         <ul></ul>
-       </div>
+      <div className={styles.content}>
+        <div className={styles.skills}>
+          {skills.map((skill, id) => (
+            <div key={id} className={styles.skill}>
+              <div className={styles.skillImageContainer}>
+                <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+              </div>
+              <p>{skill.title}</p>
+            </div>
+          ))}
+        </div>   
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
